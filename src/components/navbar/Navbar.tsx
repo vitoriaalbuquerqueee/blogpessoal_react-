31bfff270c8ac585ae2321e8eca2677f179dcd12
Generation101 <generation101@MacBook-Pro-de-Generation101.local>
2026-08-19 15:50:13 -0300

    feat: implementa a logica de login e integracao com AuthContext (Cookbook 19)

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,25 +1,36 @@
-
-import { Link } from 'react-router-dom'
+import { useContext } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../contexts/AuthContext';
 
 function Navbar() {
+  const navigate = useNavigate();
+  const { handleLogout } = useContext(AuthContext);
+
+  function logout() {
+    handleLogout();
+    alert('Usuário deslogado com sucesso');
+    navigate('/login');
+  }
+
   return (
-    <header className="w-full bg-indigo-900 text-white flex justify-center py-4">
-      <div className="container flex justify-between text-lg px-4">
-        <Link to="/home" className="text-2xl font-bold">
+    <div className="w-full bg-indigo-900 text-white flex justify-center py-4">
+      <div className="container flex justify-between text-lg">
+        <Link to="/home" className="text-2xl font-bold uppercase">
           Blog Pessoal
         </Link>
 
         <div className="flex gap-4">
-          <Link to='/home' className="text-2xl font-bold">Blog Pessoal</Link>
-          <span className="hover:underline cursor-pointer">Postagens</span>
-          <span className="hover:underline cursor-pointer">Temas</span>
-          <span className="hover:underline cursor-pointer">Cadastrar tema</span>
-          <span className="hover:underline cursor-pointer">Perfil</span>
-          <span className="hover:underline cursor-pointer">Sair</span>
+          <div className="hover:underline">Postagens</div>
+          <div className="hover:underline">Temas</div>
+          <div className="hover:underline">Cadastrar tema</div>
+          <div className="hover:underline">Perfil</div>
+          <Link to="" onClick={logout} className="hover:underline">
+            Sair
+          </Link>
         </div>
       </div>
-    </header>
-  )
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
