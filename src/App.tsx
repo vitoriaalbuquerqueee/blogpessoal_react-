9372dde350bf9972fd04034ea18404aeb8b7b0d7
Generation101 <generation101@MacBook-Pro-de-Generation101.local>
2026-08-19 15:47:28 -0300

    feat: implementa o AuthContext e AuthProvider (Cookbook 18)

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Footer from './components/footer/Footer';
 import Navbar from './components/navbar/Navbar';
+import Footer from './components/footer/Footer';
 import Home from './pages/home/Home';
-import Cadastro from './pages/cadastro/Cadastro';
 import Login from './pages/login/Login';
+import Cadastro from './pages/cadastro/Cadastro';
+import { AuthProvider } from './contexts/AuthContext';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Navbar />
-      <div className='min-h-[80vh]'>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/cadastro" element={<Cadastro />} />
-        </Routes>
-      </div>
-      <Footer />
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Navbar />
+        <div className="min-h-[80vh]">
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/cadastro" element={<Cadastro />} />
+            <Route path="/home" element={<Home />} />
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
