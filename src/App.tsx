e5ff95d263f008030da42319f5aa3a59210c9c0a
Generation101 <generation101@MacBook-Pro-de-Generation101.local>
2026-08-20 09:59:33 -0300

    style: adiciona componente FormTema e atualiza rotas (Cookbook 22)

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,27 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Navbar from './components/navbar/Navbar';
 import Footer from './components/footer/Footer';
+import Navbar from './components/navbar/Navbar';
 import Home from './pages/home/Home';
 import Login from './pages/login/Login';
-import Cadastro from './pages/cadastro/Cadastro';
-import ListaTemas from './components/listatema/ListaTemas';import { AuthProvider } from './contexts/AuthContext';
+import ListaTemas from './components/listatema/ListaTemas';
+import FormTema from './components/tema/formtema/FormTema';
 
 function App() {
   return (
-    <AuthProvider>
-      <BrowserRouter>
-        <Navbar />
-        <div className="min-h-[80vh]">
-          <Routes>
-            <Route path="/" element={<Login />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/cadastro" element={<Cadastro />} />
-            <Route path="/home" element={<Home />} />
-            <Route path="/temas" element={<ListaTemas />} />
-          </Routes>s
-        </div>
-        <Footer />
-      </BrowserRouter>
-    </AuthProvider>
+    <BrowserRouter>
+      <Navbar />
+      <div className="min-h-[80vh]">
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/temas" element={<ListaTemas />} />
+          <Route path="/cadastrartema" element={<FormTema />} />
+          <Route path="/editartema/:id" element={<FormTema />} />
+        </Routes>
+      </div>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -21,8 +21,8 @@ function Navbar() {
 
         <div className="flex gap-4">
           <div className="hover:underline">Postagens</div>
-          <div className="hover:underline">Temas</div>
-          <div className="hover:underline">Cadastrar tema</div>
+          <Link to="/temas" className="hover:underline">Temas</Link>
+          <Link to="/cadastrartema" className="hover:underline">Cadastrar tema</Link>
           <div className="hover:underline">Perfil</div>
           <Link to="" onClick={logout} className="hover:underline">
             Sair
